b94e314c89b35b50b2cf925bcd1d84aecbf2d53e
t-bittco <tasu.2331@gmail.com>
2020-04-28 01:46:57 +0900

    Top_about_実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'posts#index'
+  resources :posts, only: :index
+  resources :profile, only: :index
+  resources :works
 end
